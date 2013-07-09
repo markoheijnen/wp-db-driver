74cb341d2b272b16a90f50dc3bf7e5cd332b3c3a
Marko Heijnen <info@markoheijnen.com>
2013-07-09 13:30:02 +0200

    Check if file can be included when the plugin has been removed

--- wp-content/db.php
+++ wp-content/db.php
@@ -4,6 +4,10 @@ if ( isset( $_GET['wp-db-driver-emergency-override'] ) ) {
 	setcookie( 'wp-db-driver-emergency-override', 1, 0, '/', $_SERVER['HTTP_HOST'] );
 }
 
-if ( !isset( $_COOKIE['wp-db-driver-emergency-override'] ) && !isset( $_REQUEST['wp-db-driver-emergency-override'] ) ) {
+if (
+	! isset( $_COOKIE['wp-db-driver-emergency-override'] ) &&
+	! isset( $_REQUEST['wp-db-driver-emergency-override'] ) &&
+	is_file( WP_CONTENT_DIR . '/plugins/wp-db-driver/db.php' ) )
+{
 	require( WP_CONTENT_DIR . '/plugins/wp-db-driver/db.php' );
 }
