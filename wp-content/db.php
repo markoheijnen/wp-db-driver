9fad21fc4633f6fd4ef75f47046b07a82c84460f
kurtpayne <kpayne@gmail.com>
2013-11-21 21:36:25 -0700

    Handle custom defined WP_PLUGIN_DIR
    When a user defines WP_PLUGIN_DIR in wp-config, it will exist when the plugin is loaded.  However, if it's not defined in wp-config, then we have to fall back to WP_CONTENT_DIR/plugins.

--- wp-content/db.php
+++ wp-content/db.php
@@ -4,10 +4,15 @@ if ( isset( $_GET['wp-db-driver-emergency-override'] ) ) {
 	setcookie( 'wp-db-driver-emergency-override', 1, 0, '/', $_SERVER['HTTP_HOST'] );
 }
 
+$db_plugin_file = WP_CONTENT_DIR . '/plugins/wp-db-driver/db.php';
+if ( defined( 'WP_PLUGIN_DIR' ) ) {
+	$db_plugin_file = WP_PLUGIN_DIR . '/wp-db-driver/db.php';
+}
+
 if (
 	! isset( $_COOKIE['wp-db-driver-emergency-override'] ) &&
 	! isset( $_REQUEST['wp-db-driver-emergency-override'] ) &&
-	is_file( WP_PLUGIN_DIR . '/wp-db-driver/db.php' ) )
+	is_file( $db_plugin_file ) )
 {
-	require( WP_PLUGIN_DIR . '/wp-db-driver/db.php' );
+	require( $db_plugin_file );
 }
