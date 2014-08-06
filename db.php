6f9255a19ea46911d89343576d1173f836bb674a
Marko Heijnen <info@markoheijnen.com>
2014-08-07 01:38:32 +0200

    Rename USE_EXT_MYSQL to WP_USE_EXT_MYSQL. Fixes #44.

--- db.php
+++ db.php
@@ -508,7 +508,7 @@ class wpdb_drivers {
 		// Auto-pick the driver
 		if ( defined( 'WPDB_DRIVER' ) ) {
 			$driver = WPDB_DRIVER;
-		} elseif ( defined( 'USE_EXT_MYSQL' ) && USE_EXT_MYSQL ) {
+		} elseif ( defined( 'WP_USE_EXT_MYSQL' ) && WP_USE_EXT_MYSQL ) {
 			$driver = 'mysql';
 		} elseif ( extension_loaded( 'pdo_mysql' ) ) {
 			$driver = 'pdo_mysql';
