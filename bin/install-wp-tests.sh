416a747077c55b380d56fe3fffbe3a6f8d84dfa9
Marko Heijnen <info@markoheijnen.com>
2015-04-23 11:59:57 +0200

    Use our class to expose the methods for. See #25

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -41,6 +41,8 @@ install_test_suite() {
 	sed $ioption "s/yourusernamehere/$DB_USER/" wp-tests-config.php
 	sed $ioption "s/yourpasswordhere/$DB_PASS/" wp-tests-config.php
 	sed $ioption "s|localhost|${DB_HOST}|" wp-tests-config.php
+
+	sed $ioption "s/extends wpdb/extends wpdb_drivers/" tests/phpunit/includes/utils.php
 }
 
 install_db() {
