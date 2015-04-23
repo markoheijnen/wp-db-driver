db9fd93d7b3eae354b61b63b45549fa6d2fa47ca
Marko Heijnen <info@markoheijnen.com>
2015-04-23 11:58:22 +0200

    Have our customer error handler as a function. See #25

--- inc/error-handler.php
+++ inc/error-handler.php
@@ -8,21 +8,23 @@ function wp_set_error_handler() {
 		$errcontext = E_WARNING;
 	}
 
-	set_error_handler( function( $errno, $errstr, $errfile ) {
-		$mysql_file = dirname( dirname( __FILE__ ) ) . '/drivers/mysql.php';
+	set_error_handler( 'wp_error_handler', $errcontext );
+}
 
-		if ( $mysql_file === $errfile ) {
-			return true;
-		}
+function wp_error_handler( $errno, $errstr, $errfile ) {
+	$mysql_file = dirname( dirname( __FILE__ ) ) . '/drivers/mysql.php';
 
-		if ( preg_match( '/^(mysql_[a-zA-Z0-9_]+)/', $errstr, $matches ) ) {
-			_doing_it_wrong( $matches[1], 'Please talk to the database using $wpdb', '4.0' );
+	if ( $mysql_file === $errfile ) {
+		return true;
+	}
 
-			return apply_filters( 'wpdb_drivers_raw_mysql_call_trigger_error', true );
-		}
+	if ( preg_match( '/^(mysql_[a-zA-Z0-9_]+)/', $errstr, $matches ) ) {
+		_doing_it_wrong( $matches[1], 'Please talk to the database using $wpdb', '4.0' );
 
-		return apply_filters( 'wp_error_handler', false, $errno, $errstr, $errfile );
-	}, $errcontext );
+		return apply_filters( 'wpdb_drivers_raw_mysql_call_trigger_error', true );
+	}
+
+	return apply_filters( 'wp_error_handler', false, $errno, $errstr, $errfile );
 }
 
 wp_set_error_handler();
